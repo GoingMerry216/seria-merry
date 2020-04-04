6f19b79e1b6f815d602cfc29e428f88961c8861b
liuwenzhe <liuwenzhe1@qq.com>
2020-04-04 16:10:37 +0800

    初始化seria~

---
+++ src/test/java/com/merry216/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merry216.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
