10dd544f7fba9df58a22abe61abc4b532e7b760e
liuwenzhe <liuwenzhe1@qq.com>
2020-04-05 00:43:50 +0800

    提交转化工具空壳子

---
+++ src/main/java/com/merry216/grandis/util/MsgProcessUtil.java
@@ -0,0 +1,12 @@
+package com.merry216.grandis.util;
+
+public class MsgProcessUtil {
+    public static String getKeyword(String msg) {
+        return null;
+    }
+
+    public static String convertToString() {
+        return null;
+    }
+
+}
